065b5ca822eaf9bb6d8c8021f20e4a6cee79c37c
jmilll <jwmiller.design@gmail.com>
2021-02-18 17:41:02 -0500

    initial commit

---
+++ src/components/FormEducation3.js
@@ -0,0 +1,285 @@
+import React, { Component } from 'react'
+import uniqid from "uniqid";
+
+import CardItem from './CardItem'
+
+class FormEducation extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            eduObject: {
+                id: uniqid(),
+                school: '',
+                degree: '',
+                from: '',
+                to: '',
+                additional: '',
+            },
+            cards: [],
+            // id: {uniqid},
+            // school: '',
+            // degree: '',
+            // from: '',
+            // to: '',
+            // additional: '',
+            // cards: [],
+            // card: '',
+
+        }
+    }
+    
+    handleChange = (e) => {
+        const {name, value} = e.target 
+        //const  { eduObject } = this.state
+        //console.log([name], value)
+        this.setState({
+            //eduObject.[name] : value}
+          //eduObject: {[name]: value}
+          //eduObject: { [name]: value }
+          eduObject: {...this.state.eduObject, [name]: value}
+          //card: e.target.value,
+
+        })
+        //console.log(this.state.eduObject)
+        //console.log(Object.keys(this.state.eduObject))
+      }
+
+    onSubmitTask = (e) => {
+        e.preventDefault();
+
+        // alert(`
+        //     ${this.state.eduObject.school} ${this.state.eduObject.degree}
+        // `)
+
+        this.setState({
+            cards: this.state.cards.concat(this.state.eduObject), 
+            //eduObject: {school: 'value'}
+            
+        });
+
+        this.resetForm()
+        console.log('submited')
+        //console.log("after " + this.state.task)
+    }
+
+
+    handleChangeSchool = (e) => {
+        this.setState({
+            eduObject: {school: e.target.value}
+        })
+        console.log(this.state.eduObject)
+    }
+
+
+    resetForm = (e) => {
+        this.setState({
+            eduObject: {
+                id: uniqid(),
+                school: '',
+                degree: '',
+                from: '',
+                to: '',
+                additional: '',
+            }
+        })
+    }
+        
+    render() {
+        //const { cards } = this.state;
+
+        const renderCard = this.state.cards.map((card) => {
+            //console.log('rendercard')
+            return <CardItem 
+                    section='education2'
+                    card={card}
+                    />
+        })
+
+        return (
+        <div className='container education'>   
+
+
+            {renderCard}
+
+            <form className='education-form hidden'>
+                {/*onSubmit={this.onSubmitTask}*/}
+
+                <div className='form-section'>
+                    <label htmlFor='school'>School</label>
+                    <input 
+                        
+                        value={this.state.eduObject.school}
+                        onChange={this.handleChange}
+
+                        type='text'
+                        name='school'
+                        placeholder='Enter School'
+                        required
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='degree'>Degree</label>
+                    <input 
+                        onChange={this.handleChange}
+                        value={this.state.eduObject.degree}
+
+                        type='text'
+                        name='degree'
+                        placeholder='Enter Degree'
+                        required
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='year-from'>From</label>
+                    <input 
+                        onChange={this.handleChange}
+                        value={this.state.eduObject.from}
+
+                        //type="month"
+                        type='number'
+                        name='from'
+                        min='1940'
+                        max='2100'
+                        step='1'
+                        id='year-from'
+                        placeholder='YYYY'
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='year-to'>To</label>
+                    <input 
+                        onChange={this.handleChange}
+                        value={this.state.eduObject.to}
+
+                        //type="month"
+                        type='number'
+                        name='to'
+                        min='1940'
+                        max='2100'
+                        step='1'
+                        id='year-to'
+                        placeholder='YYYY'
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='degree-input'>Additional</label>
+                    <textarea 
+                        onChange={this.handleChange}
+                        value={this.state.eduObject.additional}
+
+                        type='text'
+                        name='additional'
+                        id='school-add'
+                        placeholder='Honors / Clubs / Acheivements'
+                    />
+                </div>
+
+                <div className='form-btns'>
+                    <button type='submit' className='submit-btn' onClick={this.onSubmitTask}>Add School</button>
+                    <button type='cancel' className='cancel-btn' onClick={this.resetForm}>Cancel</button>
+                </div>
+
+            </form>
+
+        </div> 
+        )
+    }
+}
+
+export default FormEducation
+
+
+/*            <form 
+                className='education-form hidden'
+                onSubmit={this.onSubmitTask}
+            >
+
+                <div className='form-section'>
+                    <label htmlFor='school-input'>School</label>
+                    <input 
+                        onChange={this.handleChange}
+                        value={this.state.school}
+
+                        type='text'
+                        //name='school'
+                        id='school-input' 
+                        placeholder='Enter School'
+                        required
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='degree-input'>Degree</label>
+                    <input 
+                        onChange={this.handleChange}
+                        value={this.state.degree}
+
+                        type='text'
+                        //name='degree'
+                        id='degree-input'
+                        placeholder='Enter Degree'
+                        required
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='year-from'>From</label>
+                    <input 
+                        //onChange={this.handleChange}
+                        //value={this.state.from}
+
+                        //type="month"
+                        type='number'
+                        name='from'
+                        min='1940'
+                        max='2100'
+                        step='1'
+                        id='year-from'
+                        placeholder='YYYY'
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='year-to'>To</label>
+                    <input 
+                        //onChange={this.handleChange}
+                        //value={this.state.to}
+
+                        //type="month"
+                        type='number'
+                        name='to'
+                        min='1940'
+                        max='2100'
+                        step='1'
+                        id='year-to'
+                        placeholder='YYYY'
+                    />
+                </div>
+
+                <div className='form-section'>
+                    <label htmlFor='degree-input'>Additional</label>
+                    <textarea 
+                        //onChange={this.handleChange}
+                        //value={this.state.additional}
+
+                        type='text'
+                        name='additional'
+                        id='school-add'
+                        placeholder='Honors / Clubs / Acheivements'
+                    />
+                </div>
+
+                <div className='form-btns'>
+                    <button type='submit' className='submit-btn'>Add School</button>
+                    <button type='cancel' className='cancel-btn'>Cancel</button>
+                </div>
+
+
+
+            </form>
+            */ 
