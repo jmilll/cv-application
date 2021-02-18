3e686cd3a7465738742e5aa2469ec5176081e8e3
jmilll <jwmiller.design@gmail.com>
2021-02-18 17:41:31 -0500

    education card complete

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react'
+import React from 'react'
+import uniqid from "uniqid";
 
 const CardItem = (props) => {
-    
-    // this.state = {
-    //     id: {uniqid},
-    //     school: '',
-    //     degree: '',
-    //     from: '',
-    //     to: '',
-    //     additional: '',
-    //     cards: [],
-    //     card: '',
 
-    // }
+    const { section, card } = props;
+    //const cards = props;
+    console.log(card);
 
     if (props.section === 'education') {
         return (
+
             <div className='card-item'>
                 <div className='school-info'>
                     <h5>{props.school}</h5>
@@ -30,7 +24,26 @@ const CardItem = (props) => {
             </div>
         )
 
-    } else if (props.section === 'practical experience') {
+    } else if (section === 'education2') {
+        return (
+
+            <div className='card-item'>
+                <p className='hidden'>{card.id}</p>
+
+                <div className='school-info'>
+                    <h5>{card.school}</h5>
+                    <p className='degree'>{card.degree}</p>
+                </div>
+                <div className='years'>
+                    <p>{card.from} to {card.to}</p>
+                </div>
+                <div className='additional edu'>
+                    <p className='add-text'>{card.additional}</p>
+                </div>
+            </div>
+        )
+
+    }else if (props.section === 'practical experience') {
         return (
             <div className='card-item'>
                 <h5>Practical Experience Card</h5>
