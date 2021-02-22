3fd0326438d84ff29b95c67abaf0e7b7d8345077
jmilll <jwmiller.design@gmail.com>
2021-02-22 14:26:30 -0500

    Initial Commit

---
+++ src/components/PersonalInfo.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class PersonalInfo extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+
+    render() {
+        return(
+            <div>
+                Personal Info
+            </div>
+        )
+    }
+}
+
+export default PersonalInfo
